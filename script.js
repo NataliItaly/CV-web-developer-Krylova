543230419a99628f7dc849610869cbb59b8546c2
Krylova Nataliya <natalinebo2014@yandex.ru>
2022-11-08 14:00:00 +0100

    add hover effect on projects items with JavaScript

--- script.js
+++ script.js
@@ -13,3 +13,10 @@ HEADERMENU.addEventListener("click", function () {
 const year = document.querySelector(".year");
 let now = new Date();
 year.textContent = now.getFullYear();
+
+const itemDescription = document.querySelectorAll(".item-description");
+const projectImg = document.querySelectorAll(".project-item img");
+
+for (let i = 0; i < itemDescription.length; i++) {
+  itemDescription[i].textContent = projectImg[i].getAttribute("alt");
+}
